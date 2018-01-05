52fa361ad2d26a492990e30c7e0840488922628f
Christian Abdelmassih <chrabd@kth.se>
2018-01-05 22:06:39 +0100

    try docker port-forward

--- Program.cs
+++ Program.cs
@@ -14,14 +14,18 @@ namespace TomatoAPI
         {
             ReadEnvVars();
 
-            var host = new WebHostBuilder()
+            var foo = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseUrls("http://localhost:5000")
-                .Build();
+                .UseStartup<Startup>();
 
+            if (Globals.env["DOTNET_ENV"] == "Production")
+            {
+                foo = foo.UseUrls("http://0.0.0.0:5000");
+            }
+
+            var host = foo .Build();
             host.Run();
         }
 
