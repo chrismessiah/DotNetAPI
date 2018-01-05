f4178f844f2af65dc0c78917fe601feb8d2fb7c8
Christian Abdelmassih <chrabd@kth.se>
2018-01-05 21:58:51 +0100

    move env script to Program.cs, Force port 5000

--- Program.cs
+++ Program.cs
@@ -12,14 +12,47 @@ namespace TomatoAPI
     {
         public static void Main(string[] args)
         {
+            ReadEnvVars();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:5000")
                 .Build();
 
             host.Run();
+        }
+
+        private static void ReadEnvVars()
+        {
+            if (Environment.GetEnvironmentVariable("DOTNET_ENV") == "Production")
+            {
+                var enumerator = Environment.GetEnvironmentVariables().GetEnumerator();
+                while (enumerator.MoveNext())
+                {
+                    // adds ALL env vars not only those passed by docker
+                    Globals.env.Add(enumerator.Key.ToString(), enumerator.Value.ToString());
+                }
+            }
+            else
+            {
+                string envFile = "./.env";
+                if (System.IO.File.Exists(envFile)) {
+                    string[] lines = System.IO.File.ReadAllLines(envFile);
+                    foreach (string line in lines)
+                    {
+                        var index = line.IndexOf("=");
+                        Globals.env.Add(line.Substring(0, index), line.Substring(index+1));
+                    }
+                }
+
+                if (!Globals.env.ContainsKey("DOTNET_ENV"))
+                {
+                    Globals.env.Add("DOTNET_ENV", "Development");
+                }
+            }
         }
     }
 }
