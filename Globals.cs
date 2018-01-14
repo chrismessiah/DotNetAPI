c1c824742ce69c19fb0f7c416ed8b5f3a6e4fc55
Christian Abdelmassih <chrabd@kth.se>
2018-01-14 11:20:23 +0100

    bugfix

--- Globals.cs
+++ Globals.cs
@@ -8,15 +8,19 @@ namespace DotNetAPI
         public static Dictionary<string, string> env = new Dictionary<string, string>();
         private static bool haveRead = false;
 
+        public static void ClearEnviromentVariables()
+        {
+          env = new Dictionary<string, string>();
+        }
+
         public static void ReadEnviromentVariables()
         {
             if (!haveRead) {
-                if (System.Environment.GetEnvironmentVariable("DOTNET_ENV") == "Production") {
+                ReadEnviromentVariablesDevelopment("./.env");
+                if (!Globals.env.ContainsKey("DOTNET_ENV") || Globals.env["DOTNET_ENV"] != "Development") {
+                    ClearEnviromentVariables();
                     ReadEnviromentVariablesProduction();
-                }
-                else
-                {
-                    ReadEnviromentVariablesDevelopment("./.env");
+                    Console.WriteLine("Environment is PRODUCTION");
                 }
                 haveRead = true;
             }
@@ -30,23 +34,43 @@ namespace DotNetAPI
               // adds ALL env vars not only those passed by docker
               Globals.env.Add(enumerator.Key.ToString(), enumerator.Value.ToString());
           }
+          if (Globals.env.ContainsKey("DATABASE_URL")) {
+            var tmp = Globals.env["DATABASE_URL"].Replace("postgres://", "");
+
+            var user = tmp.Split(":")[0];
+            tmp = tmp.Replace($"{user}:", "");
+
+            var password = tmp.Split("@")[0];
+            tmp = tmp.Replace($"{password}@", "");
+
+            var host = tmp.Split(":")[0];
+            tmp = tmp.Replace($"{host}:", "");
+
+            var port = tmp.Split("/")[0];
+            var db = tmp.Split("/")[1];
+
+            Globals.env["CONNECTION_STRING"] = $"User ID={user};Password={password};Server={host};Port={port};Database={db}";
+          } else {
+              Console.WriteLine("NO DATABASE LINKED !!! DATABASE_URL IS EMPTY");
+          }
         }
 
         private static void ReadEnviromentVariablesDevelopment(string path)
         {
             if (System.IO.File.Exists(path))
             {
+                Console.WriteLine("Environment is DEVELOPMENT");
                 string[] lines = System.IO.File.ReadAllLines(path);
                 foreach (string line in lines)
                 {
                     var index = line.IndexOf("=");
                     Globals.env.Add(line.Substring(0, index), line.Substring(index+1));
                 }
-            }
 
-            if (!Globals.env.ContainsKey("DOTNET_ENV"))
-            {
-                Globals.env.Add("DOTNET_ENV", "Development");
+                if (!Globals.env.ContainsKey("DOTNET_ENV"))
+                {
+                    Globals.env.Add("DOTNET_ENV", "Development");
+                }
             }
         }
     }
